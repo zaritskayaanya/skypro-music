573a2f4b01ea66cf5ddaae9f0d90d9d9c5c366db
Анна <zaritskayaanya@yandex.ru>
2025-10-22 17:42:38 +0300

    Redux Toolkit

--- src/store/features/trackSlice.ts
+++ src/store/features/trackSlice.ts
@@ -3,12 +3,10 @@ import { TrackTypes } from '../../sharedTypes/shared.Types';
 
 type initialStateType = {
   currentTrack: TrackTypes | null;
-  isPlay: boolean;
 };
 
 const initialState: initialStateType = {
   currentTrack: null,
-  isPlay: false
 };
 
 const trackSlice = createSlice({
@@ -18,11 +16,8 @@ const trackSlice = createSlice({
     setCurrentTrack: (state, action: PayloadAction<TrackTypes>) => {
     state.currentTrack = action.payload;    
     },  
-    setIsPlay:(state, action: PayloadAction<boolean>) => {
-      state.isPlay = action.payload
-    }
   },
 });
 
-export const { setCurrentTrack, setIsPlay } = trackSlice.actions;
+export const { setCurrentTrack } = trackSlice.actions;
 export const trackSliceReducer = trackSlice.reducer;
